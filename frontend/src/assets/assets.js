f823bc71cb0b583f762cc2f73fd8c38c82fbd361
muezabebe <150574720+muezabebe@users.noreply.github.com>
2026-02-20 10:58:11 -0300

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,10 +1,11 @@
 import ecommerce from './ecommerce.jpg'
 import about from './about.jpg'
 import contact from './contact.jpg'
-import stripe from './Stripe.jpg'
+//import stripe from './Stripe.jpg'
 
 
 export const assets={
  ecommerce, about,contact,stripe,
 }
+
 
